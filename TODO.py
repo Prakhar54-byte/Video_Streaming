afcfe8124f731ab59cfa2286fc6066a4d1d9d4ab
Prakhar54-byte <162185166+Prakhar54-byte@users.noreply.github.com>
2025-10-11 04:03:41 +0530

    feat: add Kafka and webhook integration for video events, update socket.io dependencies

---
+++ TODO.py
@@ -0,0 +1,12 @@
+# TODO : 18/7/25
+
+#  Backend of ffmpeg video streaming application
+## featurees in backend ( API  related for video editing )
+### 1. Video upload
+### 2. Video quality change
+### 3. Video format change
+#  
+
+
+
+## Fixing some bug s in frontend
